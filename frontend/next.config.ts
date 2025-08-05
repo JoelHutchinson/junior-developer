767877f811cfc16b5b53af1e78b4cf6ec8359cb1
Joel Hutchinson <hutchy100@live.co.uk>
2025-08-05 13:19:13 -0300

    feat: implement initial version of Content component to render content and un-cited sources

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,6 +9,16 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.google.com",
+        pathname: "/s2/favicons/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -1,6 +1,50 @@
 "use client";
 
-export default function Content() {
-    // TODO: Implement the content component to render out the content and sources
-    return <div className="border-2 border-gray-300 p-4 rounded-md">Individual content and sources should go in this component</div>;
+import Image from "next/image";
+
+import { formatCategory } from "@/utils/formatting";
+import { Data } from "@/utils/types";
+
+interface ContentProps {
+  data: Data;
+}
+
+export default function Content(props: ContentProps) {
+  return (
+    <div className="border-2 border-gray-300 p-4 rounded-md flex flex-col gap-2 bg-white text-gray-800">
+      <h3 className="font-bold underline">
+        {formatCategory(props.data.category)}
+      </h3>
+
+      <div dangerouslySetInnerHTML={{ __html: props.data.content }}></div>
+
+      <div>
+        <h4 className="font-semibold">Related articles:</h4>
+        <ul>
+          {props.data.sources
+            .filter((source) => !source.cited)
+            .map((source) => (
+              <li key={source.id} className="flex items-center gap-2">
+                {source.favicon && (
+                  <Image
+                    src={source.favicon}
+                    alt={`${source.title} favicon`}
+                    width={16}
+                    height={16}
+                  />
+                )}
+                <a
+                  href={source.source}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link-primary"
+                >
+                  {source.title}
+                </a>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
