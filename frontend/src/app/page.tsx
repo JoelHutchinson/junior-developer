4463d5af110e854a5ecafcd8ffaa15c9f138a39a
Joel Hutchinson <hutchy100@live.co.uk>
2025-08-05 13:20:32 -0300

    feat: fetch data for content rendering in Home component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import Content from "@/components/Content";
 
+import { fetchData } from "@/utils/data";
+import { Data } from "@/utils/types";
+
 export default async function Home() {
-  // TODO: Fetch the data from /api/data
+  const data = await fetchData();
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -15,14 +18,23 @@ export default async function Home() {
             height={150}
             priority
           />
-          <h1 className="text-2xl font-bold mt-4">
-            Citizens Advice SORT
-          </h1>
-          <h2 className="text-lg font-medium">
-            Junior Developer Practical
-          </h2>
+          <h1 className="text-2xl font-bold mt-4">Citizens Advice SORT</h1>
+          <h2 className="text-lg font-medium">Junior Developer Practical</h2>
         </div>
-        <Content /> {/* Each piece of content in the list should have its own content component */}
+        <div className="flex flex-wrap gap-4">
+          {
+            /* If data is available, map through it and render each piece of content */
+            // TODO: Use a key that is sure to be unique
+            data ? (
+              data.map((item: Data) => (
+                <Content key={item.category} data={item} />
+              ))
+            ) : (
+              <p className="text-gray-500">Loading content...</p>
+            )
+          }
+        </div>
+        {/* Each piece of content in the list should have its own content component */}
       </main>
     </div>
   );
