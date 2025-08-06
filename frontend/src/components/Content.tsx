933114d3d91c3c278157ecab58e79c9616c60251
Joel Hutchinson <hutchy100@live.co.uk>
2025-08-06 04:23:14 -0300

    feat: update heading styles for Advice and References sections in Content component

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -11,14 +11,14 @@ export default function Content(props: ContentProps) {
   return (
     <div className="border-2 border-gray-300 rounded-md flex flex-col gap-6 bg-white text-gray-800">
       <div className="p-4">
-        <h3 className="font-bold text-xl mb-2">Advice</h3>
+        <h3 className="font-bold text-2xl mb-4">Advice</h3>
 
         {/* NOTE: In production, we would sanitize this HTML content (ideally on the back-end) */}
         <div dangerouslySetInnerHTML={{ __html: props.data.content }}></div>
       </div>
 
       <div className="bg-blue-50 p-4">
-        <h3 className="font-bold text-xl mb-2">References</h3>
+        <h3 className="font-bold text-2xl mb-4">References</h3>
         <ul className="pl-1 space-y-1">
           {props.data.sources.map((source: EnrichedSource, index: number) => (
             <Reference key={source.id} source={source} number={index + 1} />
