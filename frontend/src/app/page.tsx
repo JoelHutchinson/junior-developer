c55f24a422dc8cb3f32d7c1948e926d21eeb8228
Joel Hutchinson <hutchy100@live.co.uk>
2025-08-06 02:43:38 -0300

    feat: refactor layout of content display for improved responsiveness

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -35,24 +35,21 @@ export default async function Home() {
           <h1 className="text-2xl font-bold mt-4">Citizens Advice SORT</h1>
           <h2 className="text-lg font-medium">Junior Developer Practical</h2>
         </div>
-        <div className="flex flex-wrap gap-8">
-          {
-            /* If data is available, map through it and render each piece of content */
-            dataGroupedByCategory ? (
-              Object.entries(dataGroupedByCategory).map(([category, items]) => (
-                <div key={category} className="flex flex-col gap-2">
-                  <h3 className="text-xl font-semibold mb-2">
-                    {formatCategory(category)}
-                  </h3>
-                  {items.map((item: EnrichedData) => (
-                    <Content key={item.id} data={item} />
-                  ))}
-                </div>
-              ))
-            ) : (
-              <p className="text-gray-500">Loading content...</p>
-            )
-          }
+        <div className="flex flex-col gap-8 w-full md:w-4/5 lg:w-3/4 mx-auto">
+          {dataGroupedByCategory ? (
+            Object.entries(dataGroupedByCategory).map(([category, items]) => (
+              <div key={category} className="flex flex-col gap-2">
+                <h3 className="text-xl font-semibold mb-2">
+                  {formatCategory(category)}
+                </h3>
+                {items.map((item: EnrichedData) => (
+                  <Content key={item.id} data={item} />
+                ))}
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-500">Loading content...</p>
+          )}
         </div>
       </main>
     </div>
