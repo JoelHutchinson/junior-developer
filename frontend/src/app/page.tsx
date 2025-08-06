6f84ffa4acc6cb46824ad7091595f05b3eedfb16
Joel Hutchinson <hutchy100@live.co.uk>
2025-08-05 21:18:07 -0300

    feat: group fetched data by category and enhance rendering logic

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,24 @@ import Image from "next/image";
 import Content from "@/components/Content";
 
 import { fetchData } from "@/utils/data";
-import { Data } from "@/utils/types";
+import { EnrichedData } from "@/utils/types";
+import { formatCategory } from "@/utils/formatting";
 
 export default async function Home() {
   const data = await fetchData();
+
+  const dataGroupedByCategory = data
+    ? data.reduce(
+        (acc: { [key: string]: EnrichedData[] }, item: EnrichedData) => {
+          if (!acc[item.category]) {
+            acc[item.category] = [];
+          }
+          acc[item.category].push(item);
+          return acc;
+        },
+        {}
+      )
+    : {};
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -21,13 +35,19 @@ export default async function Home() {
           <h1 className="text-2xl font-bold mt-4">Citizens Advice SORT</h1>
           <h2 className="text-lg font-medium">Junior Developer Practical</h2>
         </div>
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap gap-8">
           {
             /* If data is available, map through it and render each piece of content */
-            // TODO: Use a key that is sure to be unique
-            data ? (
-              data.map((item: Data) => (
-                <Content key={item.category} data={item} />
+            dataGroupedByCategory ? (
+              Object.entries(dataGroupedByCategory).map(([category, items]) => (
+                <div key={category}>
+                  <h3 className="text-xl font-semibold mb-2">
+                    {formatCategory(category)}
+                  </h3>
+                  {items.map((item: EnrichedData) => (
+                    <Content key={item.id} data={item} />
+                  ))}
+                </div>
               ))
             ) : (
               <p className="text-gray-500">Loading content...</p>
