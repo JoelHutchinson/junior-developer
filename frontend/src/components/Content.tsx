77a84bf4a992fb0fd20661521347a8e7e3d464fb
Joel Hutchinson <hutchy100@live.co.uk>
2025-08-06 02:17:57 -0300

    feat: enhance layout of Content component with improved structure and styling

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -9,12 +9,15 @@ interface ContentProps {
 
 export default function Content(props: ContentProps) {
   return (
-    <div className="border-2 border-gray-300 p-4 rounded-md flex flex-col gap-4 bg-white text-gray-800">
-      <div dangerouslySetInnerHTML={{ __html: props.data.content }}></div>
+    <div className="border-2 border-gray-300 rounded-md flex flex-col gap-6 bg-white text-gray-800">
+      <div className="p-4">
+        <h3 className="font-bold text-xl mb-2">Advice</h3>
+        <div dangerouslySetInnerHTML={{ __html: props.data.content }}></div>
+      </div>
 
-      <div>
-        <h3 className="font-semibold text-xl mb-1">References</h3>
-        <ul>
+      <div className="bg-blue-50 p-4">
+        <h3 className="font-bold text-xl mb-2">References</h3>
+        <ul className="pl-1 space-y-1">
           {props.data.sources.map((source: EnrichedSource, index: number) => (
             <Reference key={source.id} source={source} number={index + 1} />
           ))}
